5eceab64e717b674173e2228c470f8b22b3a4b50
Commander Keen <nom@ximus.de>
2009-11-10 22:39:49 +0100

    quick deploy

--- db/migrate/20091110194559_add_client_to_attack.rb
+++
@@ -1,9 +0,0 @@
-class AddClientToAttack < ActiveRecord::Migration
-  def self.up
-    add_column :attacks, :client, :string
-  end
-
-  def self.down
-    remove_column :attacks, :client
-  end
-end
